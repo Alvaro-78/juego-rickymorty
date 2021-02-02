16c9c7cbdc549ddc79ebf261fb71bfa6fbb8a4b2
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-02-02 19:16:12 +0100

    Add message logic to print team bame characters

--- js/character.js
+++ js/character.js
@@ -1,19 +1,25 @@
 
 class Character {
 
-  constructor ( name, strong, health, defense, luc ) {
+  constructor ( name, strength, health, defense, luc ) {
+
     this.name = nombre;
-    this.strong = strong;
+    this.strength = strength;
     this.health = health;
     this.defense = defense;
     this.luc = luc;
   };
 
   hit ( enemy ) {
-    enemy.health -= ( this.strong - this.defense - enemy.luc );
+    enemy.health -= ( this.strong - enemy.defense) * enemy.luc;
   };
 
   specialHit ( enemy ) {
-    enemy.health -= ( this.strong + 5 - this.defense . enemy.luc );
+    enemy.health -= ( this.strong * 0.5 - enemy.defense ) - enemy.luc;
   };
+
+  defense ( enemy ) {
+    this.defense -= ( enemy.strength  -  this.defense ) - this.luc;
+  };
+
 };
--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+let teamOne = [];
+
+let teamTwo = [];
+
+
+
 
 let selectCharacter = ( character ) => {
   
@@ -5,17 +11,25 @@ let selectCharacter = ( character ) => {
     teamOne.push( character );
     document.getElementById( character ).className = "selectedOne";
     document.getElementById( character ).onclick = "";
+    let messageTeamOne = document.getElementById("messageTeamOne");
+    messageTeamOne.innerHTML = `El primer equipo es ${teamOne} <img src="./assets/images/${ teamOne[0] }"`;
           
   }else if( teamTwo.length < 3 ) {
     teamTwo.push( character );
     document.getElementById( character ).className = "selectedTwo" ;
     document.getElementById( character ).onclick = "";
+    let messageTeamOne = document.getElementById("messageTeamOne");
+    messageTeamOne.innerHTML = `El primer equipo es ${teamOne} <img src="./assets/images/${ teamOne[0] }"`;
+
   };
+
+  let messageTeamTwo = document.getElementById("messageTeamTwo");
+
+  messageTeamTwo.innerHTML = `El segundo equipo es ${teamTwo}`;
+
   
 };
 
 
-let teamOne = [];
-
-let teamTwo = [];
-
+console.log( teamOne );
+console.log( teamTwo );
