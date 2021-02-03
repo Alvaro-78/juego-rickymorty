dd29017a9e6ed611db40f5c9a443922426735a6b
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-02-03 20:10:01 +0100

    Add choose pics in respective teams in home view

--- js/character.js
+++ js/character.js
@@ -23,3 +23,45 @@ class Character {
   };
 
 };
+
+let restartGame = () => {
+
+  let initialHealth = 200;
+  
+  player1.health = initialHealth;
+  player2.health = initialHealth;
+
+  p1 = "";
+  p2 = "";
+
+};
+
+
+
+let attack = () => {
+  //Funcion de ataque;
+  let turn = Math.floor(Math.random() * 2);
+  let special = Math.floor(Math.random() * 5);
+
+  if(turn == 0){
+      if(special == 3){
+          console.log("ATAQUE ESPECIAL");
+          player1.specialHit(player2);
+      }else{
+
+          player1.hit(player2);
+      }
+  }else{
+      if(especial == 3){
+          console.log("ATAQUE ESPECIAL");
+          player2.specialHit(player1);
+      }else{
+          player2.hit(player1);
+
+      }
+  };
+
+  console.log("Vida 1:" + player1.health);
+  console.log("Vida 2:" + player2.health);
+  
+};
--- js/main.js
+++ js/main.js
@@ -4,40 +4,56 @@ let teamTwo = [];
 
 
 
-
 let selectCharacter = ( character ) => {
   
-  p1 = character;
-
   if( teamOne.length < 3 ) {
+
     teamOne.push( character );
+
     document.getElementById( character ).className = "selectedOne";
     document.getElementById( character ).onclick = "";
-    let messageTeamOne = document.getElementById("messageTeamOne");
-    messageTeamOne.innerHTML = `El primer equipo es ${teamOne} <img src="./assets/images/${ p1 }"`;
+
+    let picOne = document.getElementById( "playerOne" );
+    picOne.innerHTML = `<div><img src="../assets/images/${ teamOne[0] }.jpg"></div>`;
+
+    let picTwo = document.getElementById( "playerTwo" );
+    picTwo.innerHTML = `<div><img src="../assets/images/${ teamOne[1] }.jpg"></div>`;
+
+    let picThree = document.getElementById( "playerThree" );
+    picThree.innerHTML = `<div><img src="../assets/images/${ teamOne[2] }.jpg"></div>`;
+
           
   }else if( teamTwo.length < 3 ) {
+
     teamTwo.push( character );
+
     document.getElementById( character ).className = "selectedTwo" ;
     document.getElementById( character ).onclick = "";
-    let messageTeamOne = document.getElementById("messageTeamOne");
-    messageTeamOne.innerHTML = `El primer equipo es ${teamOne} <img src="../assets/images/${ teamOne[0] }"`;
+    
+    let picFour = document.getElementById("playerFour");
+    picFour.innerHTML = `<div><img src="../assets/images/${ teamTwo[0] }.jpg"</div>`;
+
+    let picFive = document.getElementById("playerFive");
+    picFive.innerHTML = `<div><img src="../assets/images/${ teamTwo[1] }.jpg"</div>`;
+
+    let picSix = document.getElementById("playerSix");
+    picSix.innerHTML = `<div><img src="../assets/images/${ teamTwo[2] }.jpg"</div>`;
 
   };
 
-  let messageTeamTwo = document.getElementById("messageTeamTwo");
+  // resolveIn(4000).then(delay => {
 
-  messageTeamTwo.innerHTML = `El segundo equipo es  ${teamTwo} ,`;
-
-  resolveIn(5000).then(delay => {
-
-    switchScreen("screenHome","screenFight");
+  //   switchScreen("screenHome","screenFight");
     
-  });
+  // });
+
 };
 
-const resolveIn = delay =>
-new Promise(res => setTimeout(() => res(delay), delay));
+
+
+
+// const resolveIn = delay =>
+// new Promise(res => setTimeout(() => res(delay), delay));
 
 
 let switchScreen = (actualStage,futureStage) => {
