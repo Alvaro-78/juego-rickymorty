09302d450df8e22dde9f6a8e43c5806d6435e821
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-02-07 10:55:30 +0100

    Add attack function logic

--- js/main.js
+++ js/main.js
@@ -1,36 +1,31 @@
 class Character {
 
-  constructor ( name, health, strength, defense, luck ) {
+  constructor ( name, health, strength, defense ) {
 
     this.name = name;
     this.health = health;
     this.strength = strength;
     this.defense = defense;
-    this.luck = luck;
   };
 
   hit ( enemy ) {
-    enemy.health -= ( this.strength - enemy.defense / 1.5) + this.luck;
+    this.health -= this.strength + enemy.defense;
   };
 
   specialHit ( enemy ) {
-    enemy.health -= ( this.strength + ( this.strength / 4 ) - enemy.defense ) - enemy.luck;
-  };
-
-  defense ( enemy ) {
-    this.defense -= ( enemy.strength  -  this.defense ) - this.luck;
+    this.health -= ( this.strength + ( this.strength / 3 ) - enemy.defense );
   };
 
 };
 
-let player1 = new Character("beth", 800, 30, 20, 6);
-let player2 = new Character("hormigas_en_los_ojos", 800, 20, 30, 5);
-let player3 = new Character("jerry", 800, 35, 15, 7);
-let player4 = new Character("meeseeks", 850, 20, 30, 5);
-let player5 = new Character("morty", 800, 60,25, 7);
-let player6 = new Character("rick_pepinillo", 800, 30, 40, 8);
-let player7 = new Character("rick", 800, 20, 20, 5);
-let player8 = new Character( "summer", 800, 35, 30, 7);
+let player1 = new Character("beth", 400, 30, 20);
+let player2 = new Character("hormigas_en_los_ojos", 400, 20, 30);
+let player3 = new Character("jerry", 400, 35, 15);
+let player4 = new Character("meeseeks", 400, 20, 30);
+let player5 = new Character("morty", 400, 60,25);
+let player6 = new Character("rick_pepinillo", 400, 30, 40);
+let player7 = new Character("rick", 400, 20, 20);
+let player8 = new Character( "summer", 400, 35, 30);
 
 let p1 = '';
 let p2 = '';
@@ -56,34 +51,37 @@ let selectCharacter = ( character ) => {
     document.getElementById( character ).className = "selectedOne" ;
     document.getElementById( character ).onclick = "";
 
-    renderSelectedTeamOne();
+    renderSelectedPlayerOne();
 
-  }else { 
-
+  }else if( p2 == "" ){ 
+    
     p2 = allPlayers[character];
 
-    document.getElementById( character ).className = "selectedTwo" ;
-    document.getElementById( character ).onclick = "";
-    
-    renderSelectedTeamTwo();
+    if( allPlayers[character] = 1) {
+
+      document.getElementById( character ).className = "selectedTwo" ;
+      document.getElementById( character ).onclick = "";
+      
+    };
+
+    renderSelectedPlayerTwo();
 
   };
 
-  resolveIn(5000).then(delay => {
+  resolveIn(3000).then(delay => {
 
-    switchScreen("screenHome","screenFight");
+    switchScreenBattleCamp("screenHome","screenFight");
     
   });
   
   switchTeamsToBattle();
-  console.log(p1,p2);
 }
 
 const resolveIn = delay =>
 new Promise(res => setTimeout(() => res(delay), delay));
 
 
-let switchScreen = (actualStage,futureStage) => {
+let switchScreenBattleCamp = (actualStage,futureStage) => {
   let actualScreen = document.getElementById(actualStage);
 
   let futureScreen = document.getElementById(futureStage);
@@ -92,69 +90,101 @@ let switchScreen = (actualStage,futureStage) => {
   futureScreen.style.display = "block";
 };
 
-
-renderSelectedTeamOne = () => {
-  
-  let picOne = document.getElementById( "team1" );
-    picOne.innerHTML = `${p1.name}<div><img src="./assets/images/${ p1.name }.jpg"></div>
-    `;
-};
-
-renderSelectedTeamTwo = () => {
-  
-  let picTwo = document.getElementById("team2");
-    picTwo.innerHTML = `${p2.name}<div><img src="./assets/images/${ p2.name }.jpg"</div>`;
-}
-
 switchTeamsToBattle = () => {
 
   let picTeam1 = document.getElementById("teamBattle1");
-    picTeam1.innerHTML = `${p1.name}<div><img src="./assets/images/${ p1.name }.jpg"</div>
-    `;
+    picTeam1.innerHTML = `${p1.name}<div><img src="./assets/images/${ p1.name }.jpg"</div>`;
     
   let picTeam2 = document.getElementById("teamBattle2");
-    picTeam2.innerHTML = `${p2.name}<div><img src="./assets/images/${ p2.name }.jpg"</div>
-    `; 
-    
+    picTeam2.innerHTML = `${p2.name}<div><img src="./assets/images/${ p2.name }.jpg"</div>`; 
 };
 
+renderSelectedPlayerOne = () => {
+  
+  let picOne = document.getElementById( "team1" );
+    picOne.innerHTML = `${p1.name}<div><img src="./assets/images/${ p1.name }.jpg"></div>`;
+};
+
+renderSelectedPlayerTwo = () => {
+  
+  let picTwo = document.getElementById("team2");
+    picTwo.innerHTML = `${p2.name}<div><img src="./assets/images/${ p2.name }.jpg"</div>`;
+};
+
+
 let attack = () => {
-console.log(p1.health, p2.health)
+
   let turn = Math.floor( Math.random() * 2 );
   let special = Math.floor( Math.random() * 5 );
 
   if(turn == 0){
-      if(special == 3){
-        console.log("SPECIAAAAAL")
-          p1.specialHit( p2 );
 
-        }else{
-          console.log("ataque")
-          p1.hit( p2 );
-          p1.health = 0;
-          
-          let message = document.getElementById("teamBattle1");
-          message.innerHTML = `Estas muerto`;
-          onclick = "";
+    if(special = 2){
+      p1.specialHit( p2 );
 
-      };
+    }else{
+
+      p1.hit( p2 );
+
+    };
+    
   }else{
-      if(special == 3){
-          p2.specialHit( p1 );
 
-      }else{
-        p2.hit( p1 );
-        p2.health = 0;
+    if(special = 2){
+        p2.specialHit( p1 );
 
-        let message = document.getElementById("teamBattle2");
-        message.innerHTML = `Estas muerto`;
-        onclick = "";
+    }else{
 
-      };
+      p2.hit( p1 );
+
+      
+    };
     
   };
+  
+  battleMessage();
 
   console.log( p1.name ,':', + Math.floor(p1.health) );
   console.log( p2.name ,':', + Math.floor(p2.health) );
 
+};
+
+
+let switchScreenHome = (actualStage,futureStage) => {
+  
+  
+  let actualScreen = document.getElementById(actualStage);
+  
+  let futureScreen = document.getElementById(futureStage);
+  
+  actualScreen.style.display = "block";
+  futureScreen.style.display = "none";
+  
+  window.location.reload()
+  
+};
+
+battleMessage = () => {
+
+  if ( p1.health <= 0 ) {
+    let messageDeath = document.getElementById("teamBattle1");
+    messageDeath.innerHTML = `Estas muerto`;
+
+    let messageWin = document.getElementById("teamBattle2");
+    messageWin.innerHTML = `Eres el Amo`
+
+  }else if( p2.health <= 0){
+
+    let messageDeath = document.getElementById("teamBattle2");
+    messageDeath.innerHTML = `Estas muerto`;
+    
+    let messageWin = document.getElementById("teamBattle1");
+    messageWin.innerHTML = `Eres el Amo`;
+
+    resolveIn(2000).then(delay => {
+  
+      switchScreenHome("screenHome","screenFight");
+      
+    });
+  };
 };
